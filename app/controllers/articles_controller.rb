00a8f49b0c85244778d7670782649b5a6aa48eb8
MorganRohan <10355@coderacademy.edu.au>
2022-05-17 15:22:26 +1000

    Change to controller and show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,31 @@ class ArticlesController < ApplicationController
     def index
         @time = Time.now
         @article = Article.all
+
+        console
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+
+        console
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:page, :paragraph, :rank)
+        end
 end
